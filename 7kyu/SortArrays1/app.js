0a393259536698b19afeed5ca130235ec41ce066
imattking <matt@imattking.com>
2022-09-28 19:23:59 -0400

    bug fixes

--- 7kyu/SortArrays1/app.js
+++ 7kyu/SortArrays1/app.js
@@ -5,9 +5,9 @@ Just a simple sorting usage. Create a function that returns the elements of the 
 // input: names - unsorted array
 // output: sorted array
 sortme = function( names ){
-    //return names.sort((a, b) => a.localeCompare(b));
-    return names.sort((a,b) => a < b ? -1 : 1);
+    //return names.sort((a, b) => a.localeCompare(b)); // predifined method
+    return names.sort((a,b) => a < b ? -1 : 1); // logic behind localeCompare()
 }
 
 // Run Tests
-console.log(sortme(['one', 'two', 'three']), ['one', 'three', 'two'])
+console.log(sortme(['one', 'two', 'three']), ['one', 'three', 'two']);
