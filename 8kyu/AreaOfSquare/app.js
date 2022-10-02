2c33bccaf92a8301a67227b8695c3ef9ba22c863
imattking <matt@imattking.com>
2022-10-01 21:46:37 -0400

    coding challenges for 2022-10-01

---
+++ 8kyu/AreaOfSquare/app.js
@@ -0,0 +1,10 @@
+/*
+Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+*/
+
+function squareArea(Arc){
+    let r = (2 * A) / (Math.PI)
+    return +(r * r).toFixed(2);
+}
