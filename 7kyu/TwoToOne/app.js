eb7a3dd382da7ba36d3854d59774e5de0e958a47
imattking <matt@imattking.com>
2022-10-04 20:14:23 -0400

    coding challenges for 2022-10-04

---
+++ 7kyu/TwoToOne/app.js
@@ -0,0 +1,26 @@
+/*
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+Examples:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+*/
+
+function longest(s1, s2) {
+    let arr = (s1+s2).split('');
+    arr.sort( /*(a, b) => a.localeCompare(b)*/ );
+    arr = new Set(arr);
+    let str = '';
+    arr.forEach( e => str+= e)
+    return str;
+    // return Array.from( new Set(s1+s2) ).sort().join('');
+}
+
+console.log(longest("aretheyhere", "yestheyarehere"), 'aehrsty');
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu");
+console.log(longest("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy");
