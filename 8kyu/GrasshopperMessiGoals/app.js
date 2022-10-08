bf7951ff9ddc879945890dac3fd7a38120c92dc0
imattking <matt@imattking.com>
2022-10-08 18:27:28 -0400

    coding challenges for 2022-10-08

---
+++ 8kyu/GrasshopperMessiGoals/app.js
@@ -0,0 +1,24 @@
+/*
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+*/
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// Run Tests
+
+console.log(goals(0, 0, 0), 0);
+console.log(goals(43, 5, 10), 58)
