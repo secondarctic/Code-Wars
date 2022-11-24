1941d6da100c08d76c07d0595d72b37ba62a487d
imattking <matt@imattking.com>
2022-11-23 21:11:19 -0500

    coding challenges for 2022-11-23

---
+++ 8kyu/DoubleChar/app.js
@@ -0,0 +1,21 @@
+/* 8 Kyu - Double Char */
+/********************* */
+/* 
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+*/
+
+function doubleChar(str) {
+    return str.split('').map( e => e += e).join('');
+}
+
+// Run Tests
+
+console.log(doubleChar('abc'));
+console.log(doubleChar('String'));
+console.log(doubleChar('Hello World'));
+console.log(doubleChar('1234!'));
