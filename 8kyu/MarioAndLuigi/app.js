d8c43ffa0d8692329f277d292edabcf53190b7a2
imattking <matt@imattking.com>
2022-11-25 15:19:47 -0500

    coding challenges for 2022-11-25

---
+++ 8kyu/MarioAndLuigi/app.js
@@ -0,0 +1,29 @@
+/* 8 Kyu - Mario and Luigi */
+/************************* */
+/*
+Issue
+Looks like some hoodlum plumber and his brother have been running around and damaging your stages again.
+
+The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there are no duplicates.
+
+Task
+Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+Example
+Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+*/
+
+function pipeFix(numbers){
+    let result = []
+    for (let i = Math.min(...numbers); i <= Math.max(...numbers); i ++) {
+        result.push(i);
+    }
+    return result;
+};
+
+console.log(pipeFix([1, 2, 3, 12]));
+console.log(pipeFix([-1, 4]));
+console.log(pipeFix([1,2,3,5,6,8,9]));
+
