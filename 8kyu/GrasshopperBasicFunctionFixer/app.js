7f46c15f13d5f2e12cd4481fa86f3fe090f558db
imattking <matt@imattking.com>
2022-11-27 18:34:52 -0500

    coding challenge for 2022-11-27

---
+++ 8kyu/GrasshopperBasicFunctionFixer/app.js
@@ -0,0 +1,20 @@
+/* 8 Kyu - Grasshopper - Basic Function Fixer */
+/******************************************** */
+/* 
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+*/
+
+function addFive(num) {
+    // var total = num + 5
+    // return total;
+    return num += 5;
+}
+
+console.log(addFive(0), 5);
+console.log(addFive(5), 10);
+console.log(addFive(10), 15);
+console.log(addFive(15), 20);
+console.log(addFive(20), 25);
