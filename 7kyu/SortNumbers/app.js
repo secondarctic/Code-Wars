cd18c181715858a7d60c257eb474dcf6c3ae0975
imattking <matt@imattking.com>
2022-12-03 16:05:55 -0500

    coding challenge for 2022-12-03

---
+++ 7kyu/SortNumbers/app.js
@@ -0,0 +1,21 @@
+/* 7 Kyu - Sort Numbers */
+/********************** */
+/* 
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/
+
+function solution(nums){
+    return nums == null ? []: nums.sort((a, b) => a < b ? -1 : 1);
+}
+
+// Run Tests
+console.log(solution([1,2,3,10,5]));
+console.log(solution(null));
+console.log(solution([]));
+console.log(solution([20, 2, 10]));
+console.log(solution([2, 20, 10]));
