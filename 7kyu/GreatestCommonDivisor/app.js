f2dfee747e2074564aec4a324b6b3363c95e6058
imattking <matt@imattking.com>
2023-01-24 18:08:14 -0500

    fix logic error to find max result first

--- 7kyu/GreatestCommonDivisor/app.js
+++ 7kyu/GreatestCommonDivisor/app.js
@@ -5,11 +5,11 @@ Find the greatest common divisor of two positive integers. The integers can be l
 The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
 */
 
-// Solution w/out recursion - will time out processing larger numbers
+// Solution w/out recursion - will time out processing larger numbers if you don't remove smallest integers
 function mygcd(x, y){
     let max = Math.max(x, y);
     let arr = [];
-    for (let i = 0; i <= max; i++) {
+    for (let i = max; i >= 1; i--) {
         if (x % i === 0 && y % i === 0) {
             arr.push(i)
         }
