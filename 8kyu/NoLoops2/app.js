ca65695a186c55fa9cd9df4dc3d48b448a8bc0eb
imattking <matt@imattking.com>
2023-05-11 15:38:36 -0400

    code wars challenge for 2023 May 11

---
+++ 8kyu/NoLoops2/app.js
@@ -0,0 +1,17 @@
+/* 8kyu - No Loops 2 */
+/*
+*** No Loops Allowed ***
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+*/
+
+function check(a, x) { return a.some(e => e === x) };
+
+// Run Tests
+
+console.log(check([66, 101], 66), true);
+console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+console.log(check(['t', 'e', 's', 't'], 'e'), true);
+console.log(check(['what', 'a', 'great', 'kata'], 'kat'), false);
